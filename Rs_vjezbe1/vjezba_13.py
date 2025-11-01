0bddc3d7ff6149298a314a2c853a2ade94441b71
kalkovic21 <126813973+Kalkovic21@users.noreply.github.com>
2025-11-01 19:56:36 +0100

    Ispravljen kod

--- Rs_vjezbe1/vjezba_13.py
+++ Rs_vjezbe1/vjezba_13.py
@@ -1,35 +1,28 @@
-# 1️
 def prvi_i_zadnji(lista):
-    return (lista[0], lista[-1])
+    return lista[0], lista[-1]
 
-
-# 2️
 def maks_i_min(lista):
     maks = lista[0]
-    min_ = lista[0]
+    min = lista[0]
+    for x in lista:
+        if x > maks:
+            maks = x
+        if x < min:
+            min = x
+    return maks, min
 
-    for broj in lista:
-        if broj > maks:
-            maks = broj
-        if broj < min_:
-            min_ = broj
+def presjek(s1, s2):
+    novi = set()
+    for x in s1:
+        if x in s2:
+            novi.add(x)
+    return novi
 
-    return (maks, min_)
+lista = [1, 2, 3, 4, 5, 6]
+brojevi = [5, 10, 20, 50, 100, 3]
+s1 = {1, 2, 3, 4, 5}
+s2 = {4, 5, 6, 7, 8}
 
-#3
-def presjek(skup1, skup2):
-    rezultat = set()
-    for element in skup1:
-        if element in skup2:
-            rezultat.add(element)
-    return rezultat
-
-
-lista1 = [1, 2, 3, 4, 5, 6]
-lista2 = [5, 10, 20, 50, 100, 3]
-skup1 = {1, 2, 3, 4, 5}
-skup2 = {4, 5, 6, 7, 8}
-
-print(prvi_i_zadnji(lista1))
-print(maks_i_min(lista2))
-print(presjek(skup1, skup2))
+print(prvi_i_zadnji(lista))
+print(maks_i_min(brojevi))
+print(presjek(s1, s2))
