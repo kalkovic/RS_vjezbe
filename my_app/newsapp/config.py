ef932c8b8395df252355bcfa8116dba6787d7081
kalkovic21 <126813973+Kalkovic21@users.noreply.github.com>
2025-11-27 15:35:40 +0100

    Izmjene u config.py, storage.py.

--- my_app/newsapp/config.py
+++ my_app/newsapp/config.py
@@ -1,5 +1,1 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(BASE_DIR, "data")
-NEWS_FILE = os.path.join(DATA_DIR, "news.json")
+NEWS_FILE = "newsapp/data/news.json"
--- my_app/newsapp/data/storage.py
+++ my_app/newsapp/data/storage.py
@@ -1,10 +1,8 @@
 import json
-import os
 
 def load_news_raw():
-    path = os.path.join("newsapp", "data", "news.json")
     try:
-        with open(path, "r", encoding="utf-8") as f:
+        with open("newsapp/data/news.json", "r", encoding="utf-8") as f:
             return json.load(f)
     except:
         return []
